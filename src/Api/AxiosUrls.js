a8fffa4362fa1b885ce680f8f3eff409ae415d8f
apple <apple@apples-MacBook-Pro.local>
2022-12-12 00:11:48 +0500

    first commit

---
+++ src/Api/AxiosUrls.js
@@ -0,0 +1,78 @@
+
+//Bakery
+export const Edit = 'bakery/data/edit';
+
+export const Bakery_Data = 'bakery/data';
+
+export const Bakery_Data_Edit = 'bakery/data/edit';
+
+export const Bakery_Table = 'bakery/table';
+
+export const clear_production = 'bakery/table/clear';
+
+export const flav_name = 'bakery/flav/quat';
+
+export const Add_Movement = '/bakery/movement/add';
+
+export const Get_perct = 'bakery/percentage/get';
+
+export const Set_perct = 'bakery/percentage/set';
+
+export const Clear_perct = 'bakery/percentage/clear';
+
+export const bakery_Kiosk = 'bakery/kiosk/get';
+
+export const History = 'bakery/history';
+
+export const Notifications = '/bakery/notifications/get';
+
+export const Submit = '/bakery/today/submit';
+
+
+//Admin
+export const Login = 'auth/login';
+
+
+//Dashboard
+export const Dashboard = 'admin/dashboard/get';
+
+
+// Kiosk
+export const Kiosk = 'admin/kiosk/get';
+export const AddKiosk = '/admin/kiosk/add';
+export const EditKiosk = 'admin/kiosk/edit';
+export const DeleteKiosk = 'admin/kiosk/delete';
+
+//Employee
+export const Employee = 'admin/employee/get';
+export const AddEmployee = 'admin/employee/add';
+export const EditEmployee = 'admin/employee/edit';
+export const DeleteEmployee = 'admin/employee/delete';
+
+export const Attendance = 'admin/attendance/get';
+
+//Producht
+export const Products = 'admin/products/get';
+export const AddProducts = 'admin/products/add';
+export const EditProducts = 'admin/products/edit';
+export const DeleteProducts = 'admin/products/delete';
+
+
+// Inventory
+//Daily
+export const DailyInventoryGet = '/admin/inventory/daily/get';
+export const DailyInventoryAdd = '/admin/inventory/daily/add';
+export const DailyRequestedInv = '/admin/inventory/requested/get';
+
+//Fixed
+export const FixedInventoryGet = '/admin/inventory/fixed/get';
+export const FixedInventoryAdd = '/admin/inventory/fixed/add';
+
+//Sale Channel
+export const SaleKiosk = '/admin/salechannel/kiosk/get';
+export const SaleFoodPanda = '/admin/salechannel/food_panda/get';
+export const SaleOnlineStore = '/admin/salechannel/online/get';
+export const SaleBaskyt = '/admin/salechannel/baskyt/get';
+export const SalePandaGo = '/admin/salechannel/pandago/get';
+export const SaleBykea = '/admin/salechannel/bykea/get';
+export const OverAll = '/admin/salechannel/overall/get';
---
+++ src/Components/Admin/ATopBar/ATopBar.js
@@ -0,0 +1,29 @@
+import "./ATopBar.css"
+import logo from '../../../Assets/Images/Logo.png'
+
+import React from 'react'
+
+
+export default function ATopBar() {
+    return (
+        <div className="topbar">
+          <div className="topbarWrapper">
+            <div className="topLeft">
+              {/* <span className="logo"> */}
+                <img src={logo} alt="DWF" />
+              {/* </span> */}
+            </div>
+            {/* <div className="topRight">
+              <div className="topbarIconContainer">
+                1
+                <span className="topIconBadge">2</span>
+              </div>
+              <div className="topbarIconContainer">
+                Staff
+              </div>
+              <img src="https://images.pexels.com/photos/1526814/pexels-photo-1526814.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500" alt="" className="topAvatar" />
+            </div> */}
+          </div>
+        </div>
+      )
+}
